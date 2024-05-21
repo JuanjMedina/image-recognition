8d91ca4b77526fd3945e42f703af33a15ddd88de
Juan Medina <jmedinaguerrero847@gmail.com>
2024-05-21 14:02:00 -0500

    chore: Add AppController and AppService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RecognitionModule } from './recognition/recognition.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.services';
 
 console.log(`NODE_ENV ${process.env.NODE_ENV.trim()}`);
 const pathENV = process.env.NODE_ENV.trim();
@@ -13,5 +15,7 @@ const pathENV = process.env.NODE_ENV.trim();
     }),
     RecognitionModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
